81479871a8eeb2c477ae720d11cc7904ca3ca887
Harry <programmer6299@icloud.com>
2025-02-10 01:21:02 -0500

    Got rid of W for lncurses

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 clear
 # Compile the source code
-g++ ./code/main.cpp -o ./release/main -std=c++23 -lncursesw -DNCURSES_WIDECHAR=1 -fexec-charset=UTF-8
+g++ ./code/main.cpp -o ./release/main -std=c++23 -lncurses -DNCURSES_WIDECHAR=1 -fexec-charset=UTF-8
 
 # Run the executable if there are no errors in compilation
 if [ $? -eq 0 ]; then
